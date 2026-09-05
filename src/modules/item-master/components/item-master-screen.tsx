a5a2cf8fe1db57b300fdbbee24b158867995d4d2
Sarita-14 <korisarita.1404@gmail.com>
2026-09-05 09:34:54 +0530

    Feature Improve Pagination in Item Master

--- src/modules/item-master/components/item-master-screen.tsx
+++ src/modules/item-master/components/item-master-screen.tsx
@@ -83,7 +83,7 @@ export default function ItemMasterScreen() {
   };
 
   return (
-    <SafeAreaView style={styles.safeArea}>
+    <SafeAreaView style={styles.safeArea} edges={["top", "left", "right"]}>
       <View style={styles.header}>
         <Text style={styles.title}>Item Master</Text>
 
